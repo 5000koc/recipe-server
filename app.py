11f5fce91caf9be47182447530aefe7735126e3f
5000koc <kocran5000@gmail.com>
2023-06-16 17:51:52 +0900

    add 20230616

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_restful import Api
+from resources.recipe import RecipeListResource
+
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+api = Api(app)
+
+# 경로와 API동작코드(Resource)를 연결한다
+api.add_resource(RecipeListResource,'/recipes')
+
+if __name__ == '__main__': # 공식룰
     app.run()
 
 
